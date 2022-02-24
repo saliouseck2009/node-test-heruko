007304f329e57143889d21c2389900cb703f1021
saliou Seck <saliouseck2009@gmail.com>
2022-02-24 21:34:42 +0000

    auth

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const userCtrl = require('../conrollers/user');
 
-router.post("signup", userCtrl.signup);
-router.post("login", userCtrl.login);
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
 
 
 module.exports=router;
